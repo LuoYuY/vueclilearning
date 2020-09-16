968b721dbaeaee64324d9ce9828baf06ee91ec26
LuoYuY <15868808504@163.com>
2020-09-16 23:37:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+//a vue file : a component ([component's name] and [html codes])
+//a vue file is just an extension of a component
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+
+  </div>
+</template>
+
+<script>
+
+//  the component's name
+export default {
+  data () { // = data: function() {
+    return {
+      title:'your first vue file'
+    }
+  },
+  methods: {
+    greeting:function () {
+      return 'HELLO :)';
+
+    }
+  }
+}
+</script>
+
+
+//style controls this component
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+//:'App' comes from App.vue
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+
+  //render 提供 :render 'App' to '#app'
+  //route component: 'App'
+  render: h => h(App)
+})
